9042931d25cc2636aafc2b00763cf6a5d6e465db
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-01-16 09:08:51 -0800

    Fixed the format

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -3,4 +3,4 @@ for i in range(1, 100):
     if i == 99:
         print("{}{}".format((i // 10), (i % 10)), end="")
     else:
-        print("{}{} ,".format((i // 10), (i % 10)), end="")
+        print(" {}{},".format((i // 10), (i % 10)), end="")
