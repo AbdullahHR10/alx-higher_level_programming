1cacedd1bb1f87d16984800f690346562231b4b3
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-01-16 09:07:31 -0800

    Fixed the code

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,5 @@
-#!/usr/bin/python3
 for i in range(1, 100):
     if i == 99:
-        print("{}".format(i))
-        break
+        print("{}{}".format((i // 10), (i % 10)), end="")
     else:
-        print("{} ,".format(i), end="")
+        print("{}{} ,".format((i // 10), (i % 10)), end="")
