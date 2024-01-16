f864bb426c7a5ef51f60c4e52769ad5cf7cf31bc
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-01-16 07:25:02 -0800

    Change zero to 0

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -11,4 +11,4 @@ elif number < 6 and number % 10 != 0:
         print(f"Last digit of {number} is -{-number % 10} and is less"
               " than 6 and not 0")
 else:
-    print(f"Last digit of {number} is {number % 10} and is zero")
+    print(f"Last digit of {number} is {number % 10} and is 0")
