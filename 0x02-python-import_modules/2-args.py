79ad3c15681efe2c51c771f4725f4a40cbc9942c
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-01-18 09:25:48 -0800

    Added colons to the print statements

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,9 +5,9 @@ num_arg = len(sys.argv) - 1
 if num_arg == 0:
     print("0 arguments.")
 elif num_arg == 1:
-    print("1 argument")
+    print("1 argument:")
 else:
-    print("{} arguments".format(num_arg))
+    print("{} arguments:".format(num_arg))
 args = sys.argv[1:]
 for i, arg in enumerate(args, start=1):
     print("{}: {}".format(i, arg))
