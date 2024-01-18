d5586ce6ca32c9730c8a1182da7f5db2b7006e77
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-01-18 10:15:43 -0800

    Changed indention

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-args = sys.argv[1:]
-num_args = len(sys.argv) - 1
-if num_args > 0:
-    result = 0
-else:
-    result = sum(map(int, args))
-    print(result)
+    args = sys.argv[1:]
+    num_args = len(sys.argv) - 1
+    if num_args > 0:
+        result = 0
+    else:
+        result = sum(map(int, args))
+        print(result)
