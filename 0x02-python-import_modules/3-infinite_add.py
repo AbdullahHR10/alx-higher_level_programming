920b0f235e7d78ea5d8359e761c57aabc93c7e8c
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-01-19 05:28:30 -0800

    Wrote the code and added permissions

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
+    
     args = sys.argv[1:]
-    num_args = len(sys.argv) - 1
-    if num_args > 0:
+    number_of_args = len(sys.argv) - 1
+    if number_of_args <= 0:
         result = 0
+        print(result)
     else:
         result = sum(map(int, args))
         print(result)
+        
