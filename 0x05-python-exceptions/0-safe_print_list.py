05d6d4ecd992a7bb1b82a8d5ffb7be7a0d21c81d
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-02-05 13:46:35 -0800

    Fixed the code

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     try:
-        for i in my_list:
-            if i < 0:
-                raise ValueError("Negative numbers are not allowed")
-            print(i, end="")
+        element_count = 0
+        if x < 0:
+            raise ValueError("Negative numbers are not allowed")
+        for i in range(x):
+            if element_count >= x:
+                break
+            print(my_list[i], end="")
+            element_count += 1
+    except IndexError:
+        pass
     except ValueError as err:
         print(err)
+    print()
+    return element_count
