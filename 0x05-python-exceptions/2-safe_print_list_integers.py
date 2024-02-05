1edaf7e91a9a42c0d78b9bd461a56b3fe46e04b8
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-02-05 14:54:09 -0800

    Fixed the code

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -6,7 +6,7 @@ def safe_print_list_integers(my_list=[], x=0):
             if isinstance(item, int):
                 print("{:d}".format(item), end="")
                 count += 1
-                if count == x:
+                if count >= x:
                     break
     except (TypeError, ValueError):
         pass
