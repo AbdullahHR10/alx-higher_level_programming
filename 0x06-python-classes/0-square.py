c857c9db97735074f3d79b69accb4e88f1bdc104
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-02-06 12:49:46 -0800

    Fixed the code

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
+"""Defines a Square class"""
 class Square:
-    def __init__(self, side_length):
-        self.side_length = side_length
-
-    def area(self):
-        return self.side_length ** 2
-
-    def perimeter(self):
-        return 4 * self.side_length
+    pass
