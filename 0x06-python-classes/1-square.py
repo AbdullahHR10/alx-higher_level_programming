bd64a33ef3ee9cb1d5d92ed47eda953a826805d3
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-02-09 08:26:45 -0800

    Fixed pycodestyle

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,16 +3,16 @@
 
 
 class Square:
-        """defines a square by based on 0-square.py).
+    """defines a square by based on 0-square.py).
 
         Attributes:
             size: Size of a square.
         """
 
-        def __init__(self, size):
-            """Creates Instantiations.
+    def __init__(self, size):
+        """Creates Instantiations.
 
-            Args:
-                size: size of the square.
-            """
-            self.__size = size
+        Args:
+            size: size of the square.
+        """
+        self.__size = size
