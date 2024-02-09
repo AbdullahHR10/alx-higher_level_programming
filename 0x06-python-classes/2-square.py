3d19430ddd227d4058f008b5e7c663ff004c59af
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-02-09 09:49:02 -0800

    Documentation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,25 +1,22 @@
 #!/usr/bin/python3
 """Defines a class Square"""
 
-
 class Square:
-    """
-    Class that defines properties of square
+    """Class that defines a square by: (based on 1-square.py).
 
     Attributes:
-        size: size of a square (1 side).
+        size: Size of a square.
     """
+    
     def __init__(self, size=0):
-        """Creates new instances of square.
+        """Creates Instantiations.
 
         Args:
-            size: size of the square (1 side).
+            size: Size of the square.
         """
-        self.__size = size
 
+        self.__size = size
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        elif size < 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
