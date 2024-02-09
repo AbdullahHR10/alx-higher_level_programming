fa12382f83966654ed1f4cee8a792d893cc968d0
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-02-09 08:19:00 -0800

    Documentation

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -6,12 +6,13 @@ class Square:
         """defines a square by: (based on 0-square.py).
         
         Attributes:
-            size (int): Size of a square.
+            size: Size of a square.
         """
+
         def __init__(self, size):
             """Creates Instantiations.
             
             Args:
-                size (int): size of the square
+                size: size of the square.
             """
         self.__size = size
