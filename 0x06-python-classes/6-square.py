2bfba7726ab5d501c25df5db2bd71647c295cef8
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-02-09 12:40:11 -0800

    Fixed pycodestyle

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -59,12 +59,12 @@ class Square:
         """
         if len(value) != 2 or not isinstance(value, tuple):
             raise TypeError("position must be a tuple of 2 positive integers")
-        if not isinstance(value[0], int) or  not isinstance(value[1], int):
+        if not isinstance(value[0], int) or not isinstance(value[1], int):
             raise TypeError("position must be a tuple of 2 positive integers")
         if value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
-        
+
     def my_print(self):
         """Prints in stdout the square with the character #.
         """
