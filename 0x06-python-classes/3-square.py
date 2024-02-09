f66410947cc32488b5966e77b7160522725bc1ba
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-02-09 10:13:07 -0800

    Wrote the code

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,6 +1,3 @@
-#!/usr/bin/python3
-"""Defines a class Square"""
-
 class Square:
     """Class that defines a square by: (based on 1-square.py).
 
@@ -8,20 +5,21 @@ class Square:
         size: Size of a square.
         area: Area of a square.
     """
-    
-    def __init__(self, size=0, area=0):
+
+    def __init__(self, size=0):
         """Creates Instantiations.
 
         Args:
             size: Size of the square.
-            area: Area of the square.
         """
 
         self.__size = size
-        self.area = area
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-        def area(self):
-            return area
+
+    def area(self):
+        """Returns the area of the square.
+        """
+        return self.__size ** 2
