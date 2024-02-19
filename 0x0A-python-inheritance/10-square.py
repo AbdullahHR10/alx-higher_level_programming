c56e068897e304291fded8bb18b7486c71a759a6
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-02-19 06:44:37 -0800

    Fixed the code

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -2,26 +2,26 @@
 """Moudle of a Square that inherits from Rectangle (9-rectangle.py).
 """
 
-Rectangle = __import__('9-rectangle.py').Rectangle
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
     """Class Square.
 
     Attributes:
-    __size (int): size of the square
+        __size (int): size of the square
     """
     def __init__(self, size):
         """Initializes a Rectangle instance with the given width and height.
 
         Args:
-            size (int) size of the square.
+            __size (int): size of the square.
 
         Raises:
             TypeError: If size is not an integer.
             ValueError: If size is less than or equal to 0.
         """
-        self.integer_validator("size", size)
+        super().__init__(size, size)
         self.__size = size
 
     def area(self):
