e0c2f4371bea1749c321930659fb4dadd2bd5311
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-02-19 04:07:58 -0800

    Fixed the code

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def lookup(obj):
-    all_attrs_methods = dir(obj)
-    filtered_attrs_methods = [
-        attr for attr in all_attrs_methods
-        if not attr.startswith('_') and not callable(getattr(obj, attr))
-    ]
-    return filtered_attrs_methods
+    return [attr for attr in dir(obj) if not attr.startswith('_')]
