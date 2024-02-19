3049e2de6ba3366ed55659422459baab15cdc86f
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-02-19 06:52:57 -0800

    Wrote the code

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Moudle of a Square that inherits from Rectangle (9-rectangle.py).
+"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Class Square.
+
+    Attributes:
+        __size (int): size of the square
+    """
+    def __init__(self, size):
+        """Initializes a Rectangle instance with the given width and height.
+
+        Args:
+            __size (int): size of the square.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than or equal to 0.
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
+
+    def __str__(self):
+        return "[Square] {}/{}".format(self.__size, self.__size)
