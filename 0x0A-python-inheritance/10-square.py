741a6767d02afa7630c05a4fc69a9446bc9d0164
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-02-19 06:25:31 -0800

    Fixed the code

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -10,5 +10,14 @@ class Square(Rectangle):
     __size (int): size of the square
     """
     def __init__(self, size):
+        """Initializes a Rectangle instance with the given width and height.
+
+        Args:
+            size (int) size of the square.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than or equal to 0.
+        """
         self.integer_validator("size", size)
         self.__size = size
