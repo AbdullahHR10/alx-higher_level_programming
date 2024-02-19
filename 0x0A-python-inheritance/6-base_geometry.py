49a79ad63b0a0061d733a17407b91b74faad730b
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-02-19 05:01:46 -0800

    Wrote the code

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Moudle (based on 5-base_geometry.py).
+"""
+
+
+class BaseGeometry:
+    """An empty class.
+    """
+    def __init__(self):
+        pass
+
+@property
+def area(self):
+    raise Exception("area() is not implemented")
