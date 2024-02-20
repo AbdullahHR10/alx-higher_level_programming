8959f9d949bd55dbfbbda97e20332048da74667f
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-02-20 05:41:43 -0800

    Documentation

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+"""Module to read a file.
+"""
+
+
 def read_file(filename=""):
+    """Function that takes a filename as a parameter and reads it.
+    then prints it to stdout.
+    """
     with open(filename, 'r', encoding='utf-8') as f:
         print(f.read())
