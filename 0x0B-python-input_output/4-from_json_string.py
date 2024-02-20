e69bed12a4b77f2b433f32809c49477ebaf500b8
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-02-20 08:01:20 -0800

    Fixed the return

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -10,4 +10,4 @@ def from_json_string(my_str):
     """Function that returns an object (Python data structure)
     represented by a JSON string.
     """
-    json.loads(my_str)
+    return json.loads(my_str)
