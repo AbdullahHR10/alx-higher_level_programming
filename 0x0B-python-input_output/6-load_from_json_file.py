d6a6591d8bd6e6979ea613ea147a8a13fff4c61b
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-02-20 08:30:36 -0800

    Wrote the code

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module Tthat creates an Object from a “JSON file”.
+"""
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """Function that creates an Object from a “JSON file”.
+    """
+    json.load(filename)
