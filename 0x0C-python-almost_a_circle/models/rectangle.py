2bc4eb2d769174041c13fce71f0c785a441d5e06
AbdullahHR10 <AbdullahH.ragab10@gmail.com>
2024-02-24 01:46:51 -0800

    Test File

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -89,5 +89,5 @@ class Rectangle(Base):
 
     def __str__(self):
         """Returns the string representation of the Rectangle."""
-        return "[Rectangle] {}/{} - {}/{}".format(
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
             self.__x, self.__y, self.__width, self.__height)
