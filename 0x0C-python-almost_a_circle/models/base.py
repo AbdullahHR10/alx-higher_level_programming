213700c75690087cf7b4c11480cc789fbf93ca03
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-02-24 04:31:50 +0000

    Fixed pycodestyle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,24 +1,26 @@
-#!/usr/bin/python3
-"""Module of a base class.
+
+dule of a base class.
 """
 
 
 class Base:
-	"""Base class of all other classes in this project.
+    """Base class of all other classes in this project.
 
-	Attributes:
-		__nb_objects (int) : objects of base.
-	"""
-	__nb_objects = 0
+    Attributes:
+        __nb_objects (int): Objects of base.
+    """
 
-	def __init__(self, id=None):
-		"""Initialize Base object.
+    __nb_objects = 0
 
-		Args:
-		id (int, optional): ID of the object. Defaults to None.
-		"""
-		if id is not None:
-			self.id = id
-		else:
-			Base.__nb_objects += 1
-			self.id = Base.__nb_objects
+    def __init__(self, id=None):
+        """Initialize Base object.
+
+        Args:
+            id (int, optional): ID of the object. Defaults to None.
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
