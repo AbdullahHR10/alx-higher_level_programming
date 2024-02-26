8a6ba29cfc83f2c1d4b49ba5aff50974f922e87a
AbdullahHR10 <AbdullahH.ragab10@gmail.com>
2024-02-26 11:28:41 -0800

    Wrote the function

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -44,3 +44,10 @@ class Base:
         with open(filename, "w") as file:
             file.write(cls.to_json_string([obj.to_dictionary()
                                            for obj in list_objs]))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Returns the list of the JSON string representation json_string."""
+        if json_string is None or len(json_strong) == 0:
+            return[]
+        return json.loads(json_string)
