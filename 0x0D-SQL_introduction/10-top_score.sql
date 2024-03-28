7fb365200cb9083555cac4b3ac0247eb39a20a07
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-03-28 00:52:29 -0700

    Fixed the script

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,3 +1,2 @@
 -- Script that lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.
-USE hbtn_0c_0;
-SELECT * FROM second_table ORDER BY score ASC;
+SELECT score, name FROM second_table ORDER BY score ASC;
