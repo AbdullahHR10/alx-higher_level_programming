2e57499f216a26082d6713c307e5ce95fb5cb8fb
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-04-22 08:00:02 -0700

    Fixed semistandard style

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,11 +2,10 @@
 
 let x, i;
 if (isNaN(process.argv[2])) {
-  console.log('Missing number of occurrences')
+  console.log('Missing number of occurrences');
 } else {
   x = Number(process.argv[2]);
 }
-
 for (i = 0; i < x; i++) {
   console.log('C is fun');
 }
