0d059625496b3521fb601a40b589d5583bd18fc4
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-04-22 07:55:12 -0700

    Wrote the script

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+let x, i;
+if (isNaN(process.argv[2])) {
+  console.log('Missing number of occurrences')
+} else {
+  x = Number(process.argv[2]);
+}
+for (i = 0; i < x; i++) {
+  console.log('C is fun');
+}
