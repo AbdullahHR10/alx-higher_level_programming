237a190e058ebce25f982bfbc907dc4ad07ccc79
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-04-22 08:11:39 -0700

    Wrote the script

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+let i, j, x, row = "";
+if (isNaN(process.argv[2])) {
+  console.log('Missing size');
+} else {
+  x = Number(process.argv[2]);
+}
+for (i = 0; i < x; i++) {
+  row = "";
+  for (j = 0; j < x; j++) {
+	  row += "X";
+  }
+    console.log(row);
+}
