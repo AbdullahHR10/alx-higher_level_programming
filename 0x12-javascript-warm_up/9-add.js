c39547a4b830a1979460dfe104a6f1adf3e72203
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-04-22 09:02:17 -0700

    Wrote the script

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
 
-let a = process.argv[2];
-let b = process.argv[3];
+let a = Number(process.argv[2]);
+let b = Number(process.argv[3]);
 function add(a, b)
 {
 	console.log(a + b);
 }
+add (a, b);
