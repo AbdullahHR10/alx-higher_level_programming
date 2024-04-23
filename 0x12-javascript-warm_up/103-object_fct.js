5f4c2d986378d287d56c3c16e575374dc06d0912
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-04-22 22:20:51 -0700

    Fixed the script

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -4,10 +4,9 @@ const myObject = {
   value: 12
 };
 console.log(myObject);
-myObject.incr = function incr() {
+myObject.incr = function incr () {
   this.value++;
-}
-console.log(myObject);
+};
 myObject.incr();
 console.log(myObject);
 myObject.incr();
