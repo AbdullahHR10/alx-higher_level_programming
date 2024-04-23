62ed352785a1e10bd01b129846dc3cfd52473efe
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-04-22 20:43:54 -0700

    Wrote the script

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,5 +1,10 @@
 #!/usr/bin/node
 
-if (process.argv.length <= 2) {
+const args = process.argv.slice(2).map(Number);
+
+if (args.length <= 1) {
   console.log(0);
+} else {
+  const secondBiggest = args.sort((a, b) => b - a)[1];
+  console.log(secondBiggest);
 }
