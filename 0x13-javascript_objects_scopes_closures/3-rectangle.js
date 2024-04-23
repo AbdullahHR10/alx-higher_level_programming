3a1efd4cafaa71a19302cefd12e36532a7140592
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-04-23 09:20:11 -0700

    Fixed the class

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -5,14 +5,13 @@ module.exports = class Rectangle {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
-
-      this.print = function () {
-        let i;
-        const row = 'X'.repeat(this.width);
-        for (i = 0; i < this.height; i++) {
-          console.log(row);
-        }
-      };
     }
   }
+  print () {
+    let i;
+    const row = 'X'.repeat(this.width);
+    for (i = 0; i < this.height; i++) {
+      console.log(row);
+    }
+  };
 };
