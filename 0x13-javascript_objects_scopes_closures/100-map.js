e88aae5ff67582806b310e555ffb09dff157ef89
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-04-24 10:50:38 -0700

    Wrote the script

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const {list} = require('100-data.js');
+const {list} = require('./100-data.js');
 const multipliedList = list.map((value, index) => value * index);
 console.log(list);
 console.log(multipliedList);
