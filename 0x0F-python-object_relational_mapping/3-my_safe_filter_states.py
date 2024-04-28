18edf0f6115c80ff51bd17dbdd25d9196117f705
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-04-28 06:35:47 -0700

    Wrote the script

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Module that takes in arguments and displays all values in
+the states table of hbtn_0e_0_usa where name matches the argument
+But this time, write one that is safe from MySQL injections
+"""
+
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import State
+
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for state in session.query(State):
+        if "a" in state.name:
+            session.delete(state)
+    session.commit()
