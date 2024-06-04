0ab5d8c3465ac7c0b77c5b7246783b568fbf874b
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-06-04 07:15:24 -0700

    Write the script

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,16 +1,7 @@
 #!/usr/bin/node
 const fs = require('fs');
-const filePath = process.argv[2];
-const stringToWrite = process.argv[3];
-
-if (!filePath || !stringToWrite) {
-  process.exit(1);
-}
-
-fs.writeFile(filePath, stringToWrite, 'utf8', (err) => {
+fs.writeFile(process.argv[2], process.argv[3], err => {
   if (err) {
-      console.error(err);
-  } else {
-    console.log('The file has been saved!');
+    console.log(err);
   }
 });
