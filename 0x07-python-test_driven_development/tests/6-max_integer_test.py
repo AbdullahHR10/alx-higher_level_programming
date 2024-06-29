9bfd954bdbac8285380dbeaa48abb01ab7947808
AbdullahHR10 <AbdullahH.Ragab10>
2024-06-28 21:50:56 -0700

    Test

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,13 +1,16 @@
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
-def test_integers(self):
-    self.assertEqual([1, 2, 3, 4], 4)
-    self.assertEqual([0, 1, 15, 6], 15)
-    self.assertEqual([19, 120, 1100, 5960], 5960)
+class TestMaxInteger(unittest.TestCase):
+    def test_integers(self):
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+        self.assertEqual(max_integer([0, 1, 15, 6]), 15)
+        self.assertEqual(max_integer([19, 120, 1100, 5960]), 5960)
+    def test_negative_numbers(self):
+        self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
+        self.assertEqual(max_integer([-15, -2, -600, -1520]), -2)
+        self.assertEqual(max_integer([-1, 0, -8, -9]), 0)
+        self.assertEqual(max_integer([-1, -210, 1, -2]), 1)
 
-def test_negative_numbers(self):
-    self.assertEqual([-1, -2, -3, -4], -1)
-    self.assertEqual([-15, -2, -600, -1520], -2)
-    self.assertEqual([-1, 0, -8, -9], 0)
-    self.assertEqual([-1, -210, 1, -2], 1)
+if __name__ == '__main__':
+    unittest.main()
