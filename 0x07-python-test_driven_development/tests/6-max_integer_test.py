a68e7d9db45f0331196563706b84fb509fd9c980
AbdullahHR10 <AbdullahH.Ragab10>
2024-06-28 20:31:11 -0700

    test my test

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,13 @@
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+def test_integers(self):
+    self.assertEqual([1, 2, 3, 4], 4)
+    self.assertEqual([0, 1, 15, 6], 15)
+    self.assertEqual([19, 120, 1100, 5960], 5960)
+
+def test_negative_numbers(self):
+    self.assertEqual([-1, -2, -3, -4], -1)
+    self.assertEqual([-15, -2, -600, -1520], -2)
+    self.assertEqual([-1, 0, -8, -9], 0)
+    self.assertEqual([-1, -210, 1, -2], 1)
