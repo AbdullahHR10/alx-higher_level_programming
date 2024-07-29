37baaa7f261fb8389fb4ad1155bbff1af3fe0fb2
AbdullahHR10 <AbdullahH.Ragab10>
2024-07-29 05:36:22 -0700

    Add -o to curl

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that takes in a URL, sends a request to that URL, and displays the size of the body of the reponse
-curl -s -w '%{size_download}\n' "$1"
+curl -s -o /dev/null -w '%{size_download}\n' "$1"
