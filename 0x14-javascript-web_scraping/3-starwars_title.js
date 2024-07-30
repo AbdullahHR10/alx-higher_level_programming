0c2c09cf2b8be18650886e135b166e26d1cf2bf7
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-07-30 02:08:52 -0700

    Add task 3 script

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const request = require('request');
+const movie_id = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movie_id}`;
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  }
+  const data = JSON.parse(body);
+  console.log(data.title);
+});
