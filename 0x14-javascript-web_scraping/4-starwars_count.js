ceb00d6f28daaa9c9345bf8973770d0bf62f89e9
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-07-30 02:24:17 -0700

    Add task 4 script

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const request = require('request');
+const characterId = 18;
+const url = process.argv[2];
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  }
+  const data = JSON.parse(body);
+  const films = data.results;
+  const count = films.filter(films => films.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)).length;
+  console.log(count);
+});
