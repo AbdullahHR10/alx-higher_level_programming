718a0cf629cf8f8e70646bcceddecf823e7a760a
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-07-30 02:12:09 -0700

    Fix semistandard

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
-const movie_id = process.argv[2];
-const url = `https://swapi-api.alx-tools.com/api/films/${movie_id}`;
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 request.get(url, (error, response, body) => {
   if (error) {
     console.error(error);
