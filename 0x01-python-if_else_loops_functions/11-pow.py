eed9846bc93d42b793a02c814f3acd98b41329f2
AbdullahHR10 <AbdullahH.Ragab10>
2024-07-31 03:36:47 -0700

    Add conditions

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def pow(a, b):
-    for i in range(b):
-        a *= a
-    return a
+    result = 1
+        if b < 0:
+        a = 1 / a
+        b = -b
+    for _ in range(b):
+        result *= a
+    return result
