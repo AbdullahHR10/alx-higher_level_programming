1a940b23154303a479a8e74323fa269ae629425f
AbdullahHR10 <AbdullahH.Ragab10>
2024-07-31 03:40:35 -0700

    Use an easy logic

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,12 +1,3 @@
 #!/usr/bin/python3
 def pow(a, b):
-    result = 1
-    if b == 0:
-        return 1
-    elif b > 0:
-        for _ in range(b):
-            result *= a
-    else:
-        for _ in range(-b):
-            result /= a
-    return result
+    return a**b
