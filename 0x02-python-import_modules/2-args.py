b1e93a99536ab556b8109fdf83a2edb571c463fc
AbdullahHR10 <AbdullahH.Ragab10>
2024-07-31 22:41:47 -0700

    Add guard to prevent code execution on import

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,14 +2,12 @@
 import sys
 
 
-if len(sys.argv) == 1:
-    print("0 argumenets.")
-elif len(sys.argv) == 2:
-    print("1 argument:")
-else:
-    print("{} arguments:".format(len(sys.argv) - 1))
-for i in range(1, len(sys.argv)):
-    print("{}: {}".format(i, sys.argv[i]))
-
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) == 1:
+        print("0 argumenets.")
+    elif len(sys.argv) == 2:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(len(sys.argv) - 1))
+    for i in range(1, len(sys.argv)):
+        print("{}: {}".format(i, sys.argv[i]))
