8488fee17fb6cc94e61965227eac30c01d1305fa
AbdullahHR10 <AbdullahH.Ragab10>
2024-07-31 22:43:23 -0700

    Fix spelling

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,7 @@ import sys
 
 if __name__ == "__main__":
     if len(sys.argv) == 1:
-        print("0 argumenets.")
+        print("0 arguments.")
     elif len(sys.argv) == 2:
         print("1 argument:")
     else:
